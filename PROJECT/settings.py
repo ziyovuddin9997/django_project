2c0964a97123331e17249ac087562a1b6001d7f5
Abdullaev Ziyovuddin <ziyovuddinabdullaev9997@gmail.com>
2026-04-17 20:09:38 +0500

    uppdate settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+os.makedirs('templates', exist_ok=True)
+os.makedirs('static', exist_ok=True)
+os.makedirs('staticfiles', exist_ok=True)
+os.makedirs('media', exist_ok=True)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
